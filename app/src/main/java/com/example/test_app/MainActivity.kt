77555a70a15a342a53bb02a37a778e30fb61cf2d
krezzy <73032167+Krezux@users.noreply.github.com>
2022-09-27 02:15:44 +0300

    added notification added text change added button click event

--- app/src/main/java/com/example/test_app/MainActivity.kt
+++ app/src/main/java/com/example/test_app/MainActivity.kt
@@ -3,9 +3,24 @@ package com.example.test_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.Button // import button
+import android.widget.Toast // notificatation
+import android.widget.TextView // import text
+import org.w3c.dom.Text
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textMain = findViewById(R.id.text_Main) as TextView
+        val button_click = findViewById(R.id.button_TextChange) as Button
+
+        button_click.setOnClickListener {//  code to perform when the user clicks on the button
+
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            textMain.setText("Text Changed.")
+        }
     }
 }
+
